41316be53e053725fd24e901373c4d5cc729eb3b
g-nyberg <leenyberg94@gmail.com>
2024-08-19 12:14:49 -0400

    added function count_chars which counts char of text and returns dictonary of all chars and count of each. added tomain to print chars_count dictionary of counts of all chars

--- main.py
+++ main.py
@@ -6,10 +6,21 @@ def count_words(text):
     count = len(text.split())
     return count
 
+def count_chars(text):
+    chars = {}
+    text_lowered = text.lower()
+    for char in text_lowered:
+        if char in chars:
+            chars[char] += 1
+        else:
+            chars[char] = 1
+    return chars
+
 def main():
     path_to_book = "books/frankenstein.txt"
     text = get_book_text(path_to_book)
     word_count = count_words(text)
-    print (text,"\nTotal word count: ", word_count)
+    chars_count = count_chars(text)
+    print (text,"\nTotal word count: ", word_count,"\n", chars_count)
 
 main()
