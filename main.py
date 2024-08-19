95967541b907c39e45b021b13b0ba7f4f9278284
g-nyberg <leenyberg94@gmail.com>
2024-08-19 13:39:54 -0400

    added comments

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+# Function opens file to read
 def get_book_text(path):
     with open(path) as f:
        return  f.read()
 
+# Function counts words input string and returns count int
 def count_words(text):
     count = len(text.split())
     return count
 
+# Function counts Alpha character of a string and returns a dictionary
 def count_chars(text):
     chars = {}
     text_lowered = text.lower()
@@ -19,6 +22,7 @@ def count_chars(text):
             continue
     return chars
 
+# converts the char dictionary to a list of dictionaries for each char that is sorted my highest occurence and returns list of char dictionaries
 def convert_char_dict_list_of_dicts(char_dict):
     dicts_list = []
     for ch in char_dict:
@@ -26,9 +30,11 @@ def convert_char_dict_list_of_dicts(char_dict):
     dicts_list.sort(key = sort_on, reverse = True)
     return dicts_list
 
+# Function used for .sort() to sort on count value
 def sort_on(dict):
     return dict["count"]
 
+# Main generates and prints a report of the book in the console
 def main():
     path_to_book = "books/frankenstein.txt"
     text = get_book_text(path_to_book)
