9214338c937b74df18445f96fa94014ddd2f9aac
g-nyberg <leenyberg94@gmail.com>
2024-08-19 13:03:20 -0400

    added comments

--- main.py
+++ main.py
@@ -10,17 +10,34 @@ def count_chars(text):
     chars = {}
     text_lowered = text.lower()
     for char in text_lowered:
-        if char in chars:
-            chars[char] += 1
+        if char.isalpha():
+            if char in chars:
+                chars[char] += 1
+            else:
+                chars[char] = 1
         else:
-            chars[char] = 1
+            continue
     return chars
+
+def convert_char_dict_list_of_dicts(char_dict):
+    dicts_list = []
+    for ch in char_dict:
+        dicts_list.append({"char": ch, "count" : char_dict[ch]})
+    dicts_list.sort(key = sort_on, reverse = True)
+    return dicts_list
+
+def sort_on(dict):
+    return dict["count"]
 
 def main():
     path_to_book = "books/frankenstein.txt"
     text = get_book_text(path_to_book)
     word_count = count_words(text)
-    chars_count = count_chars(text)
-    print (text,"\nTotal word count: ", word_count,"\n", chars_count)
+    chars_count_dict = count_chars(text)
+    char_dicts_list =  convert_char_dict_list_of_dicts(chars_count_dict)
+    print (f"Report on {path_to_book}")
+    print(f"{word_count} words found in text")
+    for item in char_dicts_list:
+        print(f"Char : '{item['char']}' appeared '{item['count']}' times")
 
 main()
